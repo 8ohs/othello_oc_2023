9e6a78d6d22d5689eafcfb2c3057e972a818375e
8ohs <hachi0117@icloud.com>
2023-06-20 17:50:11 +0900

    合法手の探索方法を変更

--- RandomPlayer.cs
+++ RandomPlayer.cs
@@ -8,51 +8,19 @@ public class RandomPlayer : OthelloAI, OthelloAIInterface {
     public RandomPlayer(String name) : base(name) {}
     
     public int[] action(int[,] board, int player) {
-	int x = 0;
-	int y = 0;
-	do {
-	    x = rand.Next(0,8);
-	    y = rand.Next(0,8);
-	} while (!isStonePuttable(board, x, y, player));
+	
+	Board b = new Board();
+	b.setBoard(board);
+	int[,] gouhoute = b.getGouhoute(player);
+	int len = gouhoute.GetLength(0);
+	int index = rand.Next(0,len);	
 
 	int[] ans = new int[2];
-	ans[0] = x;
-	ans[1] = y;
+	ans[0] = gouhoute[index, 0];
+	ans[1] = gouhoute[index, 1];
 
 	//Thread.Sleep(2000); //ゆっくりにする
 	
 	return ans;
-    }
-
-    private bool isStonePuttable(int[,] board, int x, int y, int player) {
-	if (x < 0 || x > 7 || y < 0 || y > 7) return false;
-	//すでに置かれている 
-	if (board[y,x] != 0) return false;
-
-	int cp_x = x;
-	int cp_y = y;
-	
-	for (int v_x = -1; v_x <= 1; v_x++) {
-	    for (int v_y = -1 ; v_y <= 1; v_y++) {
-
-		if (v_x == 0 && v_y == 0) continue;
-		
-		cp_x = x;
-		cp_y = y;
-		cp_x += v_x;
-		cp_y += v_y;
-		if (cp_x < 0 || cp_x > 7 || cp_y < 0 || cp_y > 7) continue;
-		if (board[cp_y,cp_x] != -1*player) continue;
-
-		while (true) {
-		    cp_x += v_x;
-		    cp_y += v_y;
-		    if (cp_x < 0 || cp_x > 7 || cp_y < 0 || cp_y > 7) break;
-		    if (board[cp_y,cp_x] == 0) break;
-		    if (board[cp_y,cp_x] == player) return true;
-		}
-	    }
-	}
-	return false;
     }
 }
