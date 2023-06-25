f3fe168e63f000d6c2db1d614b8e8069502e7851
8ohs <hachi0117@icloud.com>
2023-06-25 21:16:01 +0900

    追加と修正

--- OthelloMain.cs
+++ OthelloMain.cs
@@ -12,19 +12,21 @@ public class OthelloMain {
 
 	//Human h_p1 = new Human("最弱オセロ");
 	//Human h_p2 = new Human();
-	RandomPlayer r_p1 = new RandomPlayer("ランダムくん");
+	//RandomPlayer r_p1 = new RandomPlayer("ランダムくん");
 	//RandomPlayer r_p2 = new RandomPlayer("ランダム2");
-	//MonteCarloPlayer m_p1 = new MonteCarloPlayer("もんちゃん500", 500);
-	MonteCarloPlayer2 m2_p1 = new MonteCarloPlayer2("もんちゃん2_100", 100);
+	//MonteCarloPlayer m_p1 = new MonteCarloPlayer("もんちゃん1000", 1000);
+	//MonteCarloPlayer2 m2_p1 = new MonteCarloPlayer2("もんちゃん2_100", 100);
+	//MonteCarloPlayer2 m2_p2 = new MonteCarloPlayer2("もんちゃん2_1000_20", 1000, 20);
+	MonteCarloPlayer3 m3_p1 = new MonteCarloPlayer3("モン君3_1000", 1000);
+	MonteCarloPlayer3 m3_p2 = new MonteCarloPlayer3("モン様3_1000", 1000);
 	//MonteCarloPlayer m_p2 = new MonteCarloPlayer("もんちゃん50", 50);
-	//MonteCarloPlayer m_p2 = new MonteCarloPlayer("もんちゃん2");
 	//MinPlayer min_p1 = new MinPlayer("Min君1");
 	//MinPlayer min_p2 = new MinPlayer("Min君2");
 	//CustomMon cm_p1 = new CustomMon("石塚AI");
 	//MaekuboAI mae_p1 = new MaekuboAI("前久保AI");
 	//BannoAI b_p1 = new BannoAI("バンノAI");
 
-	board.battle(m2_p1, r_p1, 30, 1);
+	board.battle(m3_p1, m3_p2, 1, 0);
 	/*
 	  battle(p1, p2, n, mode);
 	  n
@@ -35,7 +37,5 @@ public class OthelloMain {
 	  1 最終結果のみ出力
 	  2 盤面出力なし
 	 */
-
-	//battle(board, min_p2, min_p1, 1000, 2);
     }
 }
