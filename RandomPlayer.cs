e61f6236fc0396fc59f816d3fdee6034d8375e5d
8ohs <hachi0117@icloud.com>
2023-06-25 21:10:28 +0900

    モンテカルロ用のランダムプレイヤー(まったく同じ)のクラスを追加

--- RandomPlayer.cs
+++ RandomPlayer.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Threading;
 
 public class RandomPlayer : OthelloAI, OthelloAIInterface {
     private static Random rand = new Random();
@@ -8,7 +7,40 @@ public class RandomPlayer : OthelloAI, OthelloAIInterface {
     public RandomPlayer(String name) : base(name) {}
     
     public int[] action(int[,] board, int player) {
+
+	//合法手を列挙してから選ぶ
+	Board b = new Board();
+	b.setBoard(board);
+	int[,] gouhoute = b.getGouhoute(player);
+	int len = gouhoute.GetLength(0);
+	int index = rand.Next(0,len);	
+
+	//printGouhoute(gouhoute);
+	//Console.WriteLine("len={0},index={1}",len, index);
 	
+	int[] ans = new int[2];
+	ans[0] = gouhoute[index, 0];
+	ans[1] = gouhoute[index, 1];
+
+	return ans;
+    }
+
+    void printGouhoute(int[,] te) {
+	for (int i = 0; i < te.GetLength(0); i++) {
+	    Console.WriteLine("{0}:[{1},{2}]", i, te[i,0], te[i,1]);
+	}
+    }
+}
+
+public class RPForMonteCarlo : OthelloAI, OthelloAIInterface {
+    private static Random rand = new Random();
+
+    public RPForMonteCarlo() : base() {}
+    public RPForMonteCarlo(String name) : base(name) {}
+    
+    public int[] action(int[,] board, int player) {
+
+	//合法手を列挙してから選ぶ
 	Board b = new Board();
 	b.setBoard(board);
 	int[,] gouhoute = b.getGouhoute(player);
@@ -19,8 +51,6 @@ public class RandomPlayer : OthelloAI, OthelloAIInterface {
 	ans[0] = gouhoute[index, 0];
 	ans[1] = gouhoute[index, 1];
 
-	//Thread.Sleep(2000); //ゆっくりにする
-	
 	return ans;
     }
 }
